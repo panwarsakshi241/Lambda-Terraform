22f9416c5e35ab7a5316bd4c948f9bf573b37df0
sakshi panwar <sakshipanwar1912@gmail.com>
2024-04-16 18:28:44 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIAZI2LDVIMF5BMX3Z"
+  access_key = "AKIAZI2LDVIMF5VBMX3Z"
   secret_key = "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"
 }
