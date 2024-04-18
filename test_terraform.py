3613423eebef34eac9752c3025bbeb060c6d598d
Sakshi Panwar <sapanwar@deloitte.com>
2024-04-18 18:04:54 +0530

    update test_terraform.py

--- test_terraform.py
+++ test_terraform.py
@@ -2,6 +2,8 @@ import json
 
 def lambda_handler(event, context):
     print("This is the test terraform lambda function!")
+    print("Change in feature branch!!!!")
+    print("need to create pull request for dev01 branch!!!!")
     return {
             'statusCode': 200,
             'body': json.dumps('Hello from Lambda!')
