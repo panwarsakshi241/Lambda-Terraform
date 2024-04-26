f0a4335bc5d9830932127677369df33d5fa5a70b
sakshi panwar <sakshipanwar1912@gmail.com>
2024-04-26 12:31:52 +0530

    pushing python file changes

--- test_terraform.py
+++ test_terraform.py
@@ -4,6 +4,8 @@ def lambda_handler(event, context):
     print("This is the test terraform lambda function!")
     print("Change in feature branch!!!!")
     print("need to create pull request for dev01 branch!!!!")
+    print("This is the latest update!!!")
+    print("This is the 2nd latest update!!!")
     return {
             'statusCode': 200,
             'body': json.dumps('Hello from Lambda!')
