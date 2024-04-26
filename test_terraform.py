d9adcad96f3f745ea0b76e7ac595be9dc27a9a19
sakshi panwar <sakshipanwar1912@gmail.com>
2024-04-26 12:35:13 +0530

    Update test_terraform.py

--- test_terraform.py
+++ test_terraform.py
@@ -1,11 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    print("This is the test terraform lambda function!")
-    print("Change in feature branch!!!!")
-    print("need to create pull request for dev01 branch!!!!")
-    print("This is the latest update!!!")
-    print("This is the 2nd latest update!!!")
+    print("Hello This is sakshi panwar and this is my latest update on this test_terraform.py file")
     return {
             'statusCode': 200,
             'body': json.dumps('Hello from Lambda!')
